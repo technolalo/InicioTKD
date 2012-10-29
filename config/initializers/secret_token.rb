cd2409a6d2394168d44cb28d27396e878190fdef
technolalo <technolalo@gmail.com>
2012-10-28 20:49:39 -0700

    Setup rails woot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InicioTKD::Application.config.secret_token = 'af749d45a7d5ee510ee7ec486879d8be727e46065dec0d87c612a8c9f907c423c8915a242b54d1b3b785951a51647d5dae1d453ddf1296a2d885b584cf4ada3d'
